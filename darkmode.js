4ead04e047095701c0fb9b71025dc24fe2cd40c8
Siam <135020238+Siam48444@users.noreply.github.com>
2025-01-22 20:13:09 +0600

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,20 @@
+const appearance_btn = document.querySelector("#appearance_btn");
+
+
+// Check for the dark mode when the page loads
+localStorage.getItem("theme") === "darkmode" && document.body.classList.add("darkmode");
+
+
+// An interactive theme toggler
+appearance_btn.addEventListener("click", () => {
+    // Toggle between light and dark mode
+    document.body.classList.toggle("darkmode");
+
+    // Update the local storage
+    if (document.body.classList.contains("darkmode")) {
+        localStorage.setItem("theme", "darkmode");
+    }
+    else {
+        localStorage.setItem("theme", "lightmode");
+    }
+});
---
+++ index.js
@@ -0,0 +1,43 @@
+const input_box = document.querySelector("#input_box");
+const add_btn = document.querySelector("#add_btn");
+const task_container = document.querySelector("#task_container");
+
+
+// Add tasks to the task container
+add_btn.addEventListener("click", function () {
+    // Handle an empty input
+    if (input_box.value === "") {
+        input_box.setAttribute("placeholder", "Please enter a task first!");
+        return;
+    }
+
+    // Append the task to the task container
+    else {
+        var li = document.createElement("li");
+        li.textContent = input_box.value;
+        task_container.appendChild(li);  
+
+        // Append a cross to the right of the list
+        var span = document.createElement("span");
+        span.textContent = "✖";
+        li.appendChild(span);
+    }
+    
+    // Reset the input box
+    input_box.value = "";
+    input_box.setAttribute("placeholder", "Add your task...");
+});
+
+
+// Check, uncheck or remove tasks from the task container
+task_container.addEventListener("click", function(e) {
+    // Check or uncheck tasks
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked_task");
+    }
+
+    // Remove tasks
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.classList.add("removed_task");
+    }
+});
